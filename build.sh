23952adba999aa97d6f4587f59766768490647a3
Jonathan Springer <jonpspri@gmail.com>
2018-12-13 11:01:48 -0500

    Fix image name in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ glibc_version=${ADOPTOPENJDK_GLIBC_VERSION:-2.28}
 target_registry=${ADOPTOPENJDK_TARGET_REGISTRY:-adoptopenjdk}
 
 docker build --build-arg GLIBC_VERSION="$glibc_version" \
-  -t "$target_registry/openjdk_glib_builder:${glibc_version}-$(uname -m)" \
+  -t "$target_registry/openjdk-glibc-builder:${glibc_version}-$(uname -m)" \
   ./builder-image/.
 
-docker push "$target_registry/openjdk_glib_builder:${glibc_version}-$(uname -m)"
+docker push "$target_registry/openjdk-glibc-builder:${glibc_version}-$(uname -m)"
